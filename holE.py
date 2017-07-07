daddc691e248adc1174c0d861580fe05af359210
Ernest Shackle <eshackle@gmail.com>
2017-07-06 17:31:03 -0700

    Fix corrupt entity index range

--- holE.py
+++ holE.py
@@ -12,6 +12,7 @@ import sys
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.tensorboard.plugins import projector
+from tensorflow.python import debug as tf_debug
 
 
 FLAGS = None
@@ -22,11 +23,10 @@ def get_the_data():
     relation_file = 'diffbot_data/relation_ids.txt'
     corrupt_triple_file = 'diffbot_data/triples.txt'
 
-    # TODO: Build entity type dict and generate new type-safe corrupt triples each epoch
-
     entity_count = sum(1 for line in open(entity_file))
     relation_count = sum(1 for line in open(relation_file))
     triple_count = sum(1 for line in open(corrupt_triple_file))
+    print "Entities: ", entity_count, "Relations: ", relation_count, "Triples: ", triple_count
 
     with tf.name_scope('input'):
         # Load triples from triple_file TSV
@@ -44,34 +44,34 @@ def get_the_data():
         return entity_count, relation_count, triple_count, triples
 
 
-def corrupt_heads(entity_count, triples):
+def corrupt_heads(entity_count, relation_count, triples):
     # TODO lookup entityIds by type
     tail_column = tf.slice(triples, [0, 1], [-1, 1])
     relation_column = tf.slice(triples, [0, 2], [-1, 1])
-    corrupt_head_column = tf.random_uniform([FLAGS.batch_size, 1], maxval=entity_count-1, dtype=tf.int32)
+    corrupt_head_column = tf.random_uniform([FLAGS.batch_size, 1], maxval=entity_count-relation_count, dtype=tf.int32)
     concat = tf.concat([corrupt_head_column, tail_column, relation_column], 1)
     return concat
 
 
-def corrupt_tails(entity_count, triples):
+def corrupt_tails(entity_count, relation_count, triples):
     # TODO lookup entityIds by type
     head_column = tf.slice(triples, [0, 0], [-1, 1])
     relation_column = tf.slice(triples, [0, 2], [-1, 1])
-    corrupt_tail_column = tf.random_uniform([FLAGS.batch_size, 1], maxval=entity_count-1, dtype=tf.int32)
+    corrupt_tail_column = tf.random_uniform([FLAGS.batch_size, 1], maxval=entity_count-relation_count, dtype=tf.int32)
     concat = tf.concat([head_column, corrupt_tail_column, relation_column], 1)
     return concat
 
 
-def corrupt_entities(entity_count, triples):
+def corrupt_entities(entity_count, relation_count, triples):
     should_corrupt_heads = tf.less(tf.random_uniform([], 0, 1.0), .5)
     return tf.cond(should_corrupt_heads,
-                   lambda: corrupt_heads(entity_count, triples),
-                   lambda: corrupt_tails(entity_count, triples))
+                   lambda: corrupt_heads(entity_count, relation_count, triples),
+                   lambda: corrupt_tails(entity_count, relation_count, triples))
 
 
 def corrupt_relations(relation_count, triples):
     entity_columns = tf.slice(triples, [0, 0], [-1, 2])
-    corrupt_relation_column = tf.random_uniform([FLAGS.batch_size, 1], maxval=relation_count-1, dtype=tf.int32)
+    corrupt_relation_column = tf.random_uniform([FLAGS.batch_size, 1], maxval=relation_count, dtype=tf.int32)
     return tf.concat([entity_columns, corrupt_relation_column], 1)
 
 
@@ -79,7 +79,7 @@ def corrupt_batch(entity_count, relation_count, triples):
     should_corrupt_relation = tf.less(tf.random_uniform([], 0, 1.0), .5)
     return tf.cond(should_corrupt_relation,
                    lambda: corrupt_relations(relation_count, triples),
-                   lambda: corrupt_entities(entity_count, triples))
+                   lambda: corrupt_entities(entity_count, relation_count, triples))
 
 
 def circular_correlation(h, t):
@@ -89,11 +89,12 @@ def circular_correlation(h, t):
         return h - t
 
     # these ops are GPU only!
-    return tf.ifft(tf.multiply(tf.conj(tf.fft(h)), tf.fft(t)))
+    with tf.device('/gpu:0'):
+        return tf.ifft(tf.multiply(tf.conj(tf.fft(h)), tf.fft(t)))
 
 
-def init_embedding(projector_config, name, relation_count, entity_count, embedding_dim):
-    embedding = tf.get_variable(name, [relation_count + entity_count, 2 * embedding_dim],
+def init_embedding(projector_config, name, entity_count, embedding_dim):
+    embedding = tf.get_variable(name, [entity_count, 2 * embedding_dim],
                                 initializer=tf.random_normal_initializer)
 
     embeddings_config = projector_config.embeddings.add()
@@ -103,11 +104,12 @@ def init_embedding(projector_config, name, relation_count, entity_count, embeddi
 
 
 def get_embedding(layer_name, entity_ids, embeddings, embedding_dim):
-    entity_embeddings = tf.nn.embedding_lookup(embeddings, entity_ids, max_norm=1)
-    reshaped = tf.reshape(entity_embeddings, [-1, 2 * embedding_dim])
-    return tf.complex(tf.slice(reshaped, [0, 0], [-1, embedding_dim]),
-                      tf.slice(reshaped, [0, embedding_dim], [-1, embedding_dim]),
-                      name=layer_name)
+    with tf.device('/cpu:0'):
+        entity_embeddings = tf.nn.embedding_lookup(embeddings, entity_ids, max_norm=1)
+        reshaped = tf.reshape(entity_embeddings, [-1, 2 * embedding_dim])
+        return tf.complex(tf.slice(reshaped, [0, 0], [-1, embedding_dim]),
+                          tf.slice(reshaped, [0, embedding_dim], [-1, embedding_dim]),
+                          name=layer_name)
 
 
 def complex_tanh(complex_tensor):
@@ -155,6 +157,7 @@ def run_training(entity_count, relation_count, triple_count, triples):
     learning_rate = FLAGS.learning_rate
     batch_size = FLAGS.batch_size
     batch_count = triple_count / batch_size
+    print "Embedding dimension: ", embedding_dim, "Batch size: ", batch_size, "Batch count: ", batch_count
 
     # Warning: this will clobber existing summaries
     if not FLAGS.resume_checkpoint and os.path.isdir(FLAGS.output_dir):
@@ -167,7 +170,7 @@ def run_training(entity_count, relation_count, triple_count, triples):
 
     # Initialize embeddings (TF doesn't support complex embeddings, split real part and imaginary part)
     projector_config = projector.ProjectorConfig()
-    embeddings = init_embedding(projector_config, 'embeddings', relation_count, entity_count, embedding_dim)
+    embeddings = init_embedding(projector_config, 'embeddings', entity_count, embedding_dim)
 
     with tf.name_scope('batch'):
         # Sample triples
@@ -187,24 +190,29 @@ def run_training(entity_count, relation_count, triple_count, triples):
         # Score and minimize hinge-loss
         loss = tf.maximum(train_loss - corrupt_loss + margin, 0)
         # TODO: experiment with other optimizers
-        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
         # Log the total batch loss
         total_loss = tf.reduce_sum(loss)
         tf.summary.scalar('loss', total_loss)
 
+    summaries = tf.summary.merge_all()
+    init_op = tf.global_variables_initializer()
+
     # Save embeddings
     saver = tf.train.Saver({'embeddings': embeddings})
 
-    with tf.Session() as sess:
-        summaries = tf.summary.merge_all()
+    supervisor = tf.train.Supervisor(logdir=FLAGS.output_dir)
+    with supervisor.managed_session() as sess:
+        if FLAGS.debug:
+            sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+            sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
+
         summary_writer = tf.summary.FileWriter(FLAGS.output_dir + '/graph', sess.graph)
         projector.visualize_embeddings(summary_writer, projector_config)
 
-        init_op = tf.global_variables_initializer()
-        sess.run(init_op)
-
         # Load the previous model if resume_checkpoint=True
+        sess.run(init_op)
         if FLAGS.resume_checkpoint:
             saver.restore(sess, FLAGS.output_dir + '/model.ckpt')
             # TODO: continue counting from last epoch
@@ -214,7 +222,7 @@ def run_training(entity_count, relation_count, triple_count, triples):
 
         try:
             epoch = 0
-            while not coord.should_stop():
+            while not supervisor.should_stop():
                 epoch += 1
                 batch_losses = []
                 for batch in range(1, batch_count):
@@ -227,7 +235,7 @@ def run_training(entity_count, relation_count, triple_count, triples):
                                                           run_metadata=run_metadata)
                         step = '{}-{}'.format(epoch, batch)
                         summary_writer.add_run_metadata(run_metadata, step)
-                        summary_writer.add_summary(summary)
+                        supervisor.summary_computed(sess, summary)
                         print "\tSaved summary for step " + step
                     else:
                         _, batch_loss = sess.run([optimizer, total_loss])
@@ -303,6 +311,11 @@ if __name__ == '__main__':
         '--resume_checkpoint',
         action='store_true',
         help='Resume training on the checkpoint model. Otherwise start with randomly initialized embeddings.'
+    )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='Run with interactive Tensorflow debugger.'
     )
 
     FLAGS, unparsed = parser.parse_known_args()
