bab8aa308cc6e9fa691885798c0d908a24499bdf
Ernest Shackle <eshackle@gmail.com>
2017-07-28 17:51:27 -0700

    Minor code clean up

--- holE.py
+++ holE.py
@@ -548,114 +548,25 @@ def main(_):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--cpu',
-        action='store_true',
-        help='Disable GPU-only operations (namely FFT/iFFT).'
-    )
-    parser.add_argument(
-        '--learning_rate',
-        type=float,
-        default=0.1,
-        help='Initial learning rate.'
-    )
-    parser.add_argument(
-        '--learning_decay_steps',
-        type=float,
-        default=16,
-        help='Learning rate decay steps (in epochs).'
-    )
-    parser.add_argument(
-        '--learning_decay_rate',
-        type=float,
-        default=0.5,
-        help='Learning decay rate.'
-    )
-    parser.add_argument(
-        '--batch_size',
-        type=int,
-        default=512,
-        help='Batch size.'
-    )
-    parser.add_argument(
-        '--num_epochs',
-        type=int,
-        default=1000,
-        help='Number of training epochs.'
-    )
-    parser.add_argument(
-        '--embedding_dim',
-        type=int,
-        default=128,
-        help='Embedding dimension.'
-    )
-    parser.add_argument(
-        '--log_loss',
-        action='store_true',
-        help='Use logistic loss. Otherwise, use pairwise ranking loss.'
-    )
-    parser.add_argument(
-        '--negative_ratio',
-        type=int,
-        default=4,
-        help='Number of negative labels to sample for each positive label (log_loss only).'
-    )
-    parser.add_argument(
-        '--margin',
-        type=float,
-        default=1.,
-        help='Hinge loss margin.'
-    )
-    parser.add_argument(
-        '--padded_size',
-        type=int,
-        default=100000,
-        help='The maximum number of entities to use for each type while sampling corrupt triples.'
-    )
-    parser.add_argument(
-        '--output_dir',
-        type=str,
-        default='holE-latest',
-        help='Tensorboard Summary directory.'
-    )
-    parser.add_argument(
-        '--data_dir',
-        type=str,
-        default='diffbot_data/kg_0.01',
-        help='Input data directory.'
-    )
-    parser.add_argument(
-        '--reader_threads',
-        type=int,
-        default=4,
-        help='Number of training triple file readers.'
-    )
-    parser.add_argument(
-        '--resume_checkpoint',
-        action='store_true',
-        help='Resume training on the checkpoint model. Otherwise start with randomly initialized embeddings.'
-    )
-    parser.add_argument(
-        '--debug',
-        action='store_true',
-        help='Run with interactive Tensorflow debugger.'
-    )
-    parser.add_argument(
-        '--infer',
-        action='store_true',
-        help='Infer new triples from the latest checkpoint model.'
-    )
-    parser.add_argument(
-        '--infer_typesafe',
-        action='store_true',
-        help='Infer new triples using type-safe candidate entities only.'
-    )
-    parser.add_argument(
-        '--inference-threshold',
-        type=float,
-        default=0.8,
-        help='Infer new triples from the latest checkpoint model.'
-    )
+    parser.add_argument('--cpu', action='store_true', help='Disable GPU-only operations (namely FFT/iFFT).')
+    parser.add_argument('--learning_rate', type=float, default=0.1, help='Initial learning rate.')
+    parser.add_argument('--learning_decay_steps', type=float, default=16, help='Learning rate decay steps (in epochs).')
+    parser.add_argument('--learning_decay_rate', type=float, default=0.5, help='Learning decay rate.')
+    parser.add_argument('--batch_size', type=int, default=512, help='Batch size.')
+    parser.add_argument('--num_epochs', type=int, default=1000, help='Number of training epochs.')
+    parser.add_argument('--embedding_dim', type=int, default=128, help='Embedding dimension.')
+    parser.add_argument('--log_loss', action='store_true', help='Use logistic loss istead of pairwise ranking loss.')
+    parser.add_argument('--negative_ratio', type=int, default=1, help='Number of negative labels sampled in log_loss.')
+    parser.add_argument('--margin', type=float, default=1., help='Hinge loss margin.')
+    parser.add_argument('--padded_size', type=int, default=100000,
+                        help='The maximum number of entities to use for each type while sampling corrupt triples.')
+    parser.add_argument('--output_dir', type=str, default='holE-latest', help='Tensorboard Summary directory.')
+    parser.add_argument('--data_dir', type=str, default='diffbot_data/kg_0.01', help='Input data directory.')
+    parser.add_argument('--reader_threads', type=int, default=4, help='Number of training triple file readers.')
+    parser.add_argument('--resume_checkpoint', action='store_true', help='Resume training on the checkpoint model.')
+    parser.add_argument('--debug', action='store_true', help='Run with interactive Tensorflow debugger.')
+    parser.add_argument('--infer', action='store_true', help='Infer new triples from the latest checkpoint model.')
+    parser.add_argument('--infer_typesafe', action='store_true', help='Infer triples using type-safe candidates.')
 
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
